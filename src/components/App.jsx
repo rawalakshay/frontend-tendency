63477695c19fc03428a4573355102a80acf6fc2c
arawal <akshayrawal.itm@gmail.com>
2023-01-23 12:18:55 +0530

    modal added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Table, Container, Pagination } from '@nextui-org/react';
+import { Table, Container, Pagination, Button, Spacer, Modal, Text, Input } from '@nextui-org/react';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
@@ -24,6 +24,15 @@ const App = () => {
     ];
 
     const [rows, setRows] = useState([]);
+    const [visible, setVisible] = useState(false)
+
+    const closeHandler = () => {
+        setVisible(false);
+    }
+
+    const openHandler = () => {
+        setVisible(true);
+    }
 
     useEffect(() => {
         axios.get('/api/data').then((response) => {
@@ -44,34 +53,98 @@ const App = () => {
     return (
         <>
             <Container xl css={{ maxWidth: "60%" }}>
-                <Table
-                    bordered
-                    aria-label="Example table with dynamic content"
-                    css={{
-                        height: "auto",
-                    }}
+                <div>
+                    <Spacer y={0.5} />
+
+                    <Button auto color="primary" onClick={openHandler}>
+                        Add Order
+                    </Button>
+                    <Spacer y={0.5} />
+                    <Table
+                        bordered
+                        aria-label="Example table with dynamic content"
+                        css={{
+                            height: "auto",
+                        }}
+                    >
+                        <Table.Header columns={columns}>
+                            {(column) => (
+                                <Table.Column key={column.key}>{column.label}</Table.Column>
+                            )}
+                        </Table.Header>
+                        <Table.Body items={rows}>
+                            {(item) => (
+                                <Table.Row key={item.key}>
+                                    {(columnKey) => <Table.Cell>{item[columnKey]}</Table.Cell>}
+                                </Table.Row>
+                            )}
+                        </Table.Body>
+                        <Table.Pagination
+                            noMargin
+                            align="center"
+                            rowsPerPage={5}
+                            onPageChange={(page) => console.log({ page })}
+                        />
+                    </Table>
+                </div>
+
+                <Modal
+                    closeButton
+                    blur
+                    aria-labelledby="modal-title"
+                    open={visible}
+                    onClose={closeHandler}
                 >
-                    <Table.Header columns={columns}>
-                        {(column) => (
-                            <Table.Column key={column.key}>{column.label}</Table.Column>
-                        )}
-                    </Table.Header>
-                    <Table.Body items={rows}>
-                        {(item) => (
-                            <Table.Row key={item.key}>
-                                {(columnKey) => <Table.Cell>{item[columnKey]}</Table.Cell>}
-                            </Table.Row>
-                        )}
-                    </Table.Body>
-                    <Table.Pagination
-                        shadow
-                        noMargin
-                        align="center"
-                        rowsPerPage={5}
-                        onPageChange={(page) => console.log({ page })}
-                    />
-                </Table>
-                {/* <Pagination total={5} initialPage={1} totalItems={rows.length} /> */}
+                    <Modal.Header>
+                        <Text id="modal-title" size={18}>
+                            <Text b size={18}>
+                                Add a New Order
+                            </Text>
+                        </Text>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Input
+                            clearable
+                            bordered
+                            fullWidth
+                            color="primary"
+                            size="lg"
+                            placeholder="SKU"
+                        />
+                        <Input
+                            clearable
+                            bordered
+                            fullWidth
+                            color="primary"
+                            size="lg"
+                            placeholder="Name"
+                        />
+                         <Input
+                            clearable
+                            bordered
+                            fullWidth
+                            color="primary"
+                            size="lg"
+                            placeholder="Quantity"
+                        />
+                        <Input
+                            clearable
+                            bordered
+                            fullWidth
+                            color="primary"
+                            size="lg"
+                            placeholder="Price"
+                        />
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button auto flat color="error" onPress={closeHandler}>
+                            Close
+                        </Button>
+                        <Button auto onPress={closeHandler}>
+                            Add Order
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </Container>
         </>
 
