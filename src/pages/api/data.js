79309f756bdf97958581274908d5217a0b2ce46e
arawal <akshayrawal.itm@gmail.com>
2023-01-23 16:16:53 +0530

    hard code auth token

--- src/pages/api/data.js
+++ src/pages/api/data.js
@@ -4,7 +4,7 @@ export default function data(req, res) {
 
     axios.get('https://eshop-deve.herokuapp.com/api/v2/orders', {
         headers: {
-            authorization: process.env.ESHOP_TOKEN
+            authorization: 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJwUGFINU55VXRxTUkzMDZtajdZVHdHV3JIZE81cWxmaCIsImlhdCI6MTYyMDY2Mjk4NjIwM30.lhfzSXW9_TC67SdDKyDbMOYiYsKuSk6bG6XDE1wz2OL4Tq0Og9NbLMhb0LUtmrgzfWiTrqAFfnPldd8QzWvgVQ'
         }
     })
         .then(response => {
