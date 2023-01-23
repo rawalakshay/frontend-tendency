33bbdedee81bdd4ba00c26f466a6af40ee26dc78
arawal <akshayrawal.itm@gmail.com>
2023-01-23 13:43:21 +0530

    form validation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,6 +25,12 @@ const App = () => {
 
     const [rows, setRows] = useState([]);
     const [visible, setVisible] = useState(false)
+    const [sku, setSku] = useState("");
+    const [name, setName] = useState("");
+
+    const [quantity, setQuantity] = useState("");
+    const [price, setPrice] = useState("");
+    const [validForm, setValidForm] = useState(false);
 
     const closeHandler = () => {
         setVisible(false);
@@ -33,6 +39,115 @@ const App = () => {
     const openHandler = () => {
         setVisible(true);
     }
+
+    let nameVar, quantityVar, priceVar, skuVar;
+
+    const setSkuFn = (event) => {
+        skuVar = event.target.value;
+        setSku(event.target.value);
+        checkFormValidation();
+    }
+
+    const setNameFn = (event) => {
+        nameVar = event.target.value;
+        setName(event.target.value);
+        checkFormValidation();
+    }
+
+    const setQuantityFn = (event) => {
+        quantityVar = event.target.value;
+        setQuantity(event.target.value);
+        checkFormValidation();
+    }
+
+    const setPriceFn = (event) => {
+        priceVar = event.target.value;
+        setPrice(event.target.value);
+        checkFormValidation();
+    }
+
+    const addNewOrder = () => {
+        if (sku == "") {
+            console.log(`Empty SKU`);
+        }
+    }
+
+    const checkFormValidation = () => {
+        if (nameVar == "") {
+            setValidForm(true);
+        }
+        if (quantityVar == "") {
+            setValidForm(true);
+        }
+        if (priceVar == "") {
+            setValidForm(true);
+        }
+        if (skuVar == "") {
+            setValidForm(true);
+        }
+
+        if(nameVar != '' && quantityVar != '' && priceVar != '' && skuVar != '') {
+            console.log('form is valid')
+            setValidForm(false);
+        }
+
+    }
+
+    const skuHelper = React.useMemo(() => {
+        if (sku == '')
+            return {
+                text: "Enter SKU",
+                color: "error",
+            };
+        else {
+            return {
+                text: "",
+                color: "success",
+            };
+        }
+    }, [sku]);
+
+    const nameHelper = React.useMemo(() => {
+        if (name == '')
+            return {
+                text: "Enter Name",
+                color: "error",
+            };
+        else {
+            return {
+                text: "",
+                color: "success",
+            };
+        }
+    }, [name]);
+
+    const quantityHelper = React.useMemo(() => {
+        if (quantity == '')
+            return {
+                text: "Enter Qauntity",
+                color: "error",
+            };
+        else {
+            return {
+                text: "",
+                color: "success",
+            };
+        }
+    }, [quantity]);
+
+    const priceHelper = React.useMemo(() => {
+        if (price == '')
+            return {
+                text: "Enter Price",
+                color: "error",
+            };
+        else {
+            return {
+                text: "",
+                color: "success",
+            };
+        }
+    }, [price]);
 
     useEffect(() => {
         axios.get('/api/data').then((response) => {
@@ -55,7 +170,6 @@ const App = () => {
             <Container xl css={{ maxWidth: "60%" }}>
                 <div>
                     <Spacer y={0.5} />
-
                     <Button auto color="primary" onClick={openHandler}>
                         Add Order
                     </Button>
@@ -107,40 +221,55 @@ const App = () => {
                             clearable
                             bordered
                             fullWidth
-                            color="primary"
                             size="lg"
                             placeholder="SKU"
+                            onChange={setSkuFn}
+                            color={skuHelper.color}
+                            helperText={skuHelper.text}
+                            helperColor={skuHelper.color}
                         />
+                        <Spacer y={0.1} />
                         <Input
                             clearable
                             bordered
                             fullWidth
-                            color="primary"
                             size="lg"
                             placeholder="Name"
+                            onChange={setNameFn}
+                            color={nameHelper.color}
+                            helperText={nameHelper.text}
+                            helperColor={nameHelper.color}
                         />
-                         <Input
-                            clearable
-                            bordered
-                            fullWidth
-                            color="primary"
-                            size="lg"
-                            placeholder="Quantity"
-                        />
+                        <Spacer y={0.1} />
                         <Input
                             clearable
                             bordered
                             fullWidth
-                            color="primary"
+                            size="lg"
+                            placeholder="Quantity"
+                            onChange={setQuantityFn}
+                            color={quantityHelper.color}
+                            helperText={quantityHelper.text}
+                            helperColor={quantityHelper.color}
+                        />
+                        <Spacer y={0.1} />
+                        <Input
+                            clearable
+                            bordered
+                            fullWidth
                             size="lg"
                             placeholder="Price"
+                            onChange={setPriceFn}
+                            color={priceHelper.color}
+                            helperText={priceHelper.text}
+                            helperColor={priceHelper.color}
                         />
                     </Modal.Body>
                     <Modal.Footer>
                         <Button auto flat color="error" onPress={closeHandler}>
                             Close
                         </Button>
-                        <Button auto onPress={closeHandler}>
+                        <Button auto onClick={addNewOrder} disabled={validForm}>
                             Add Order
                         </Button>
                     </Modal.Footer>
